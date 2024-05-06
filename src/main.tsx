53777109b490477157094278ed277d147055d0c5
Marat <68328146+Ramramramzes@users.noreply.github.com>
2024-05-06 12:07:29 +0300

    store & router

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store/store.ts'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<App />} />
+    </>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
+  <Provider store={store}>
+    <React.StrictMode>
+    <RouterProvider router={router} />
   </React.StrictMode>,
+  </Provider>
 )
