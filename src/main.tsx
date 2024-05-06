e6e455ca9090a6640150838786abf64e05c89172
Marat <68328146+Ramramramzes@users.noreply.github.com>
2024-05-06 12:10:36 +0300

    login page create

--- src/main.tsx
+++ src/main.tsx
@@ -18,6 +18,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </React.StrictMode>
   </Provider>
 )
