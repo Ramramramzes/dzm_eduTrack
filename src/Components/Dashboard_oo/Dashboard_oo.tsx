fae05683dd5609fd903d35a1302fd4fbe6a0116f
Marat <68328146+Ramramramzes@users.noreply.github.com>
2024-05-12 21:31:40 +0300

    making own component for dash_oo

---
+++ src/Components/Dashboard_oo/Dashboard_oo.tsx
@@ -0,0 +1,80 @@
+import styles from './dashboard_oo.module.css';
+import { GetPrograms } from '../../hooks/getPrograms';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../../store/store';
+import { backStatus, roundColor } from '../../style/_dashboard';
+import { IMainSpec } from '../../services/getMainSpec';
+import { Link, useNavigate } from 'react-router-dom';
+import { setPopup } from '../../store/dashboardState';
+import { Popup } from '../Popup';
+import { useEffect } from 'react';
+import { getProfile } from '../../services/getProfile';
+import { setProfile } from '../../store/userState';
+import { getAdress } from '../../services/getAdress';
+import { setDefAdress } from '../../store/defaultDataState';
+
+export function Dashboard_oo() {
+  const dispatch = useDispatch<AppDispatch>();
+  const DashboardState = useSelector((state: RootState) => state.dashboard);
+  const LoginState = useSelector((state: RootState) => state.login);
+  const UserState = useSelector((state: RootState) => state.user);
+  const DefaultData = useSelector((state: RootState) => state.default);
+  const ProfileState = useSelector((state: RootState) => state.newProfile);
+  const navigate = useNavigate();
+
+  const orgId = LoginState.defaultData?.[0]?.org_id ? LoginState.defaultData[0].org_id : 0
+  const programms =  GetPrograms(orgId)
+
+  
+  useEffect(() => {
+    if(orgId === 0){
+      navigate('/')
+    }
+  },[navigate, orgId])
+
+  useEffect(() => {
+    const fetchData = async() =>{
+      const profile = await getProfile(UserState.id)
+      dispatch(setProfile(profile))
+    }
+
+    fetchData()
+  },[])
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const adress = await getAdress(UserState.id)
+      dispatch(setDefAdress(adress))
+    }
+
+    if(ProfileState.user_id === 0){
+      fetchData()
+    }
+  },[UserState.id, dispatch, DashboardState.popup, ProfileState.user_id])
+
+
+  return (
+    <>
+      <h1>Программы {UserState && UserState.profile && UserState.profile.short_name}</h1>
+      <div className={styles.cardsBlock}>
+        {programms && programms.map((programm) => {
+          return  <Link to='/programm' state={programm.programm_id} className={styles.card} key={programm.programm_id}>
+                    <h3>{programm.name}</h3>
+                    <p>Время обучения {programm.hours} ч.</p>
+                    <div>
+                      <p>Основная специальность: {DefaultData.mainSpec.map((main:IMainSpec) => {
+                                                    return main.value === programm.spec_main ? main.name : ''
+                                                  })}</p>
+                    <div className={styles.statusBlock} style={backStatus(programm.status === 200 ? '#00FF00' : programm.status === 100 ? '#FFFF00' : '#FF0000')}>
+                      {programm.status === 200 ? 'Программа активна' : programm.status === 100 ? 'Программа в обработке' : 'Программа требует исправления'}
+                      <div className={styles.neon} style={roundColor(programm.status === 200 ? '#00FF00' : programm.status === 100 ? '#FFFF00' : '#FF0000')}></div>
+                    </div>
+                    </div>
+                  </Link>
+        })}
+      </div>
+      {DashboardState.popup && <Popup />}
+      <button onClick={() => {dispatch(setPopup(true))}}>Добавить программу</button>
+    </>
+  );
+}
